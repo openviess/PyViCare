808c2c0bcf89604d0abdcb3a325e8e6237cb057a
Simon Gillet <simon@gillet.ninja>
2019-01-10 16:02:22 +0100

    Implemented most of the setters

--- PyViCare.py
+++ PyViCare.py
@@ -10,8 +10,14 @@ token_url = 'https://iam.viessmann.com/idp/v1/token';
 apiURLBase = 'https://api.viessmann-platform.io';
 redirect_uri = "vicare://oauth-callback/everest";
 viessmann_scope=["openid"]
-# TODO Holiday program can still be used (parameters are there)
-# TODO set eco program (still there as well)
+
+# TODO Holiday program can still be used (parameters are there) heating.circuits.0.operating.programs.holiday
+# TODO set eco program (does not appear to work on my install)
+# TODO heating.dhw.schedule/setSchedule slope&shift numbers
+# TODO heating.circuits.0.heating.curve/setCurve
+# TODO error handling
+# TODO documentation
+# TODO re-use token (not generate a new one each time)
 
 class PyViCare:
     
@@ -31,15 +37,12 @@ class PyViCare:
             header = {'Content-Type': 'application/x-www-form-urlencoded'}
             response = requests.post(authorization_url, headers=header,auth=(username, password))
         except Exception as e:
-            #print e
             #capture the error, which contains the code the authorization code and put this in to codestring
             codestring = "{0}".format(str(e.args[0])).encode("utf-8");
             codestring = str(codestring)
             match = re.search("code\=(.*)\&",codestring)
             codestring=match.group(1)
         oauth.fetch_token(token_url, client_secret=client_secret,authorization_response=authorization_url,code=codestring)
-        #r = oauth.get(apiURLBase+"/general-management/installations?expanded=true&")
-        #print r.content
         return oauth
         
     def getInstallations(self):
@@ -50,27 +53,6 @@ class PyViCare:
         self.serial=self.installations["entities"][0]["entities"][0]["properties"]["serial"]
         return self.installations
     
-    def jsonParsing(self,dct):
-        if 'isEnabled' in dct:
-            return dct["isEnabled"]
-        if 'properties' in dct:
-            return dct["properties"]
-        if 'temperature' in dct:
-            return dct["temperature"]
-        if 'value' in dct:
-            return dct["value"]
-        if 'active' in dct:
-            return dct["active"]
-        if 'status' in dct:
-            return dct["status"]
-        return dct
-    
-    def getProperty2(self,property_name):
-        url = apiURLBase + '/operational-data/installations/' + str(self.id) + '/gateways/' + str(self.serial) + '/devices/0/features/' + property_name
-        r=self.oauth.get(url)
-        j=json.loads(r.content,object_hook=self.jsonParsing)
-        return j
-    
     def getProperty(self,property_name):
         url = apiURLBase + '/operational-data/installations/' + str(self.id) + '/gateways/' + str(self.serial) + '/devices/0/features/' + property_name
         r=self.oauth.get(url)
@@ -78,10 +60,33 @@ class PyViCare:
         return j
     
     def setProperty(self,property_name,action,data):
-        h = {"Content-Type":"application/json"}
-        url = apiURLBase + '/operational-data/installations/' + str(self.id) + '/gateways/' + str(self.serial) + '/devices/0/features/' + property_name +"/"+action
+        h = {"Content-Type":"application/json","Accept":"application/vnd.siren+json"}
+        url = apiURLBase + '/operational-data/v1/installations/' + str(self.id) + '/gateways/' + str(self.serial) + '/devices/0/features/' + property_name +"/"+action
         r=self.oauth.post(url,data,headers=h)
         print(r.content)
+        
+    def setMode(self,mode):
+        self.setProperty("heating.circuits.0.operating.modes.active","setMode","{\"mode\":\""+mode+"\"}")
+    
+    # Works for normal, reduced, comfort
+    # active has no action
+    # exetenral , standby no action
+    # holiday, sheculde and unscheduled
+    # activate, decativate comfort,eco
+    
+    def setProgramTemperature(self,program,temperature):
+        self.setProperty("heating.circuits.0.operating.programs."+program,"setTemperature","{\"targetTemperature\":"+str(temperature)+"}")
+    
+    # comfort, eco
+    # optional temperature parameter could be passed (but not done)
+    def activateProgram(self,program):
+        self.setProperty("heating.circuits.0.operating.programs."+program,"activate","{}")
+        
+    def deactivateProgram(self,program):
+        self.setProperty("heating.circuits.0.operating.programs."+program,"deactivate","{}")
+    
+    def setDomesticHotWaterTemperature(self,temperature):
+        self.setProperty("heating.dhw.temperature","setTargetTemperature","{\"temperature\":"+str(temperature)+"}")
     
     def getMonthSinceLastService(self):
         return self.getProperty("heating.service.timeBased")["properties"]["activeMonthSinceLastService"]["value"]
@@ -96,8 +101,6 @@ class PyViCare:
     def getSupplyTemperature(self):
         return self.getProperty("heating.circuits.0.sensors.temperature.supply")["properties"]["value"]["value"]
     
-    # Implement error
-    # TODO
     def getRoomTemperature(self):
         return self.getProperty("heating.circuits.0.sensors.temperature.room")["properties"]["value"]["value"]
         
