f63300fe56e82f6a4e055c9a08b6754276461af2
Lukas Wöhrl <lukas@woehrl.net>
2021-01-02 01:19:32 +0100

    Renamed test to reflect generic device

--- tests/test_GenericDevice.py
+++ tests/test_GenericDevice.py
@@ -1,29 +1,36 @@
 import unittest
 from tests.ViCareServiceForTesting import ViCareServiceForTesting
-from PyViCare.PyViCareGazBoiler import GazBoiler
+from PyViCare.PyViCareDevice import Device
 
-class GasConfiguration(unittest.TestCase):
+class GenericDevice(unittest.TestCase):
     def setUp(self):
         self.service = ViCareServiceForTesting(None, 0, {})
-        self.gaz = GazBoiler(None, None, None, 0, 0, self.service)
+        self.device = Device(None, None, None, 0, 0, self.service)
         
     def test_activateComfort(self):
-        self.gaz.activateComfort()
+        self.device.activateComfort()
         self.assertEqual(len(self.service.setPropertyData), 1)
         self.assertEqual(self.service.setPropertyData[0]['action'], 'activate')
         self.assertEqual(self.service.setPropertyData[0]['property_name'], 'heating.circuits.0.operating.programs.comfort')
 
     def test_deactivateComfort(self):
-        self.gaz.deactivateComfort()
+        self.device.deactivateComfort()
         self.assertEqual(len(self.service.setPropertyData), 1)
         self.assertEqual(self.service.setPropertyData[0]['action'], 'deactivate')
         self.assertEqual(self.service.setPropertyData[0]['property_name'], 'heating.circuits.0.operating.programs.comfort')
 
     def test_setDomesticHotWaterTemperature(self):
-        self.gaz.setDomesticHotWaterTemperature(50)
+        self.device.setDomesticHotWaterTemperature(50)
         self.assertEqual(len(self.service.setPropertyData), 1)
         self.assertEqual(self.service.setPropertyData[0]['property_name'], 'heating.dhw.temperature')
         self.assertEqual(self.service.setPropertyData[0]['action'], 'setTargetTemperature')
         self.assertEqual(self.service.setPropertyData[0]['data'], '{"temperature":50}')
+
+    def test_setMode(self):
+        self.device.setMode('dhw')
+        self.assertEqual(len(self.service.setPropertyData), 1)
+        self.assertEqual(self.service.setPropertyData[0]['property_name'], 'heating.circuits.0.operating.modes.active')
+        self.assertEqual(self.service.setPropertyData[0]['action'], 'setMode')
+        self.assertEqual(self.service.setPropertyData[0]['data'], '{"mode":"dhw"}')
 
     
