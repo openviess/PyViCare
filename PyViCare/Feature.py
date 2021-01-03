f5651c15741a0f723764ecd5311ee5db328130da
Lukas Wöhrl <lukas@woehrl.net>
2021-01-03 21:51:49 +0100

    Optimize comments about the feature flag

--- PyViCare/Feature.py
+++ PyViCare/Feature.py
@@ -1,2 +1,4 @@
-#feature flag to have a release which still returns an error string
+# Feature flag to raise an exception in case of a non existing device feature.
+# The flag should be fully removed in a later release. 
+# It allows dependend libraries to gracefully migrate to the new behaviour
 raise_exception_instead_of_returning_error = False
