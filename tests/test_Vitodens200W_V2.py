69fcbbd627bc1440d2652043c3106f07b54f6ef1
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-07-13 17:31:56 +0200

    Add api for solar collector (#98)
    * Add api for solar collector
    
    * Whitespace
    
    * Add tests for solar collector temp and pump

--- tests/test_Vitodens200W_V2.py
+++ tests/test_Vitodens200W_V2.py
@@ -10,7 +10,7 @@ class Vitodens200WV2(unittest.TestCase):
         self.service = ViCareServiceMockV2('response_Vitodens200W_V2.json', 0)
         self.gaz = GazBoiler(None, None, None, 0, 0, self.service)
         PyViCare.Feature.raise_exception_on_not_supported_device_feature = True
-        
+
     def test_getBurnerActive(self):
         self.assertEqual(self.gaz.getBurnerActive(), False)
 
@@ -36,3 +36,9 @@ class Vitodens200WV2(unittest.TestCase):
     def test_ensure_old_behavior_non_supported_feature_returns_error(self):
         PyViCare.Feature.raise_exception_on_not_supported_device_feature = False
         self.assertEqual(self.gaz.getPowerConsumptionDays(), "error")
+
+    def test_getSolarCollectorTemperature(self):
+        self.assertEqual(self.gaz.getSolarCollectorTemperature(), 55.3)
+
+    def test_getSolarPumpActive(self):
+        self.assertEqual(self.gaz.getSolarPumpActive(), False)
