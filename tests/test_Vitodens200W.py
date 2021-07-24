892b5f58970e1c3b20e997e117aa006295b45eac
Lukas Wöhrl <lukas@woehrl.net>
2021-07-24 14:01:25 +0200

    Fixed read of power consumption and updates response (#118)

--- tests/test_Vitodens200W.py
+++ tests/test_Vitodens200W.py
@@ -11,35 +11,28 @@ class Vitodens200W(unittest.TestCase):
         PyViCare.Feature.raise_exception_on_not_supported_device_feature = True
 
     def test_getBurnerActive(self):
-        self.assertEqual(self.device.getBurnerActive(), False)
+        self.assertEqual(self.device.getBurnerActive(), True)
 
     def test_getBurnerStarts(self):
-        self.assertEqual(self.device.getBurnerStarts(), 17169)
+        self.assertEqual(self.device.getBurnerStarts(), 8028)
 
     def test_getBurnerHours(self):
-        self.assertEqual(self.device.getBurnerHours(), 1589.3)
+        self.assertEqual(self.device.getBurnerHours(), 5570)
 
     def test_getBurnerModulation(self):
-        self.assertEqual(self.device.getBurnerModulation(), 0)
-
-    def test_getPowerConsumptionDays_fails(self):
-        self.assertRaises(PyViCareNotSupportedFeatureError, self.device.getPowerConsumptionDays)
+        self.assertEqual(self.device.getBurnerModulation(), 11.1)
 
     def test_getPrograms(self):
-        expected_programs = ['active', 'comfort', 'eco', 'external', 'holiday', 'normal', 'reduced', 'standby']
+        expected_programs = ['active', 'comfort', 'forcedLastFromSchedule', 'normal', 'reduced', 'standby']
         self.assertListEqual(self.device.getPrograms(), expected_programs)
 
     def test_getModes(self):
-        expected_modes = ['standby', 'dhw', 'dhwAndHeating', 'forcedReduced', 'forcedNormal']
+        expected_modes =  ['standby', 'heating', 'dhw', 'dhwAndHeating']
         self.assertListEqual(self.device.getModes(), expected_modes)
 
-    def test_getPrograms(self):
-        expected_modes = ['active', 'comfort', 'eco', 'external', 'holiday', 'normal', 'reduced', 'standby']
-        self.assertListEqual(self.device.getPrograms(), expected_modes)
-
-    def test_ensure_old_behavior_non_supported_feature_returns_error(self):
-        PyViCare.Feature.raise_exception_on_not_supported_device_feature = False
-        self.assertEqual(self.device.getPowerConsumptionDays(), "error")
+    def test_getPowerConsumptionDays(self):
+        expected_consumption = [0, 0.1, 0.2, 0.1, 0.2, 0.2, 0.2, 0.2]
+        self.assertEqual(self.device.getPowerConsumptionDays(), expected_consumption)
 
     def test_getFrostProtectionActive(self):
         self.assertEqual(self.device.getFrostProtectionActive(), False)
