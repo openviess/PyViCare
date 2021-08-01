1e6d07c87b7498d0b2fc6913f6414623c352a9b2
Lukas Wöhrl <lukas@woehrl.net>
2021-08-01 15:16:02 +0200

    Add setting of dhw circulation schedule (#124)

--- PyViCare/PyViCareDevice.py
+++ PyViCare/PyViCareDevice.py
@@ -339,6 +339,13 @@ class Device:
     def activateOneTimeCharge(self):
         return self.service.setProperty("heating.dhw.oneTimeCharge","activate","{}")
 
+    def setDomesticHotWaterCirculationSchedule(self,schedule):
+        return self.service.setProperty("heating.dhw.pumps.circulation.schedule", "setSchedule","{\"newSchedule\":"+str(schedule)+"}")
+
+    @handleNotSupported
+    def getDomesticHotWaterCirculationSchedule(self):
+        return self.service.getProperty("heating.dhw.pumps.circulation.schedule")["commands"]["setSchedule"]["params"]["newSchedule"]["constraints"]["modes"]
+      
     @handleNotSupported
     def getAvailableCircuits(self):
         return self.service.getProperty("heating.circuits")["properties"]["enabled"]["value"]
