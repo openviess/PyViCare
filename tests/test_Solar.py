fc6f40301b9c3d5da91f5583f23d542cabae1780
Lukas Wöhrl <lukas@woehrl.net>
2021-09-04 21:08:53 +0200

    Add additional solar methods to tests

--- tests/test_Solar.py
+++ tests/test_Solar.py
@@ -10,8 +10,18 @@ class SolarTest(unittest.TestCase):
         self.device = Device(self.service)
 
     def test_getSolarStorageTemperature(self):
-        self.assertEqual(self.device.getSolarStorageTemperature(), 63.5)
+        self.assertEqual(self.device.getSolarStorageTemperature(), 41.5)
 
     def test_getSolarPowerProduction(self):
         self.assertEqual(
-            self.device.getSolarPowerProduction(), [16.604, 19.78, 19.323, 20.592, 19.444, 14.517, 17.929, 20.534])
+            self.device.getSolarPowerProduction(), [19.773, 20.642, 18.831, 22.672, 18.755, 14.513, 15.406, 13.115])
+
+    def test_getSolarCollectorTemperature(self):
+        self.assertEqual(self.device.getSolarCollectorTemperature(), 21.9)
+    
+    def test_getSolarPumpActive(self):
+        self.assertEqual(self.device.getSolarPumpActive(), False)
+
+        
+
+    
