9732b97bfbbdc67506c558d29c608c5488ef3197
Lukas Wöhrl <lukas@woehrl.net>
2021-09-05 21:14:43 +0200

    extract methods

--- tests/test_TestData.py
+++ tests/test_TestData.py
@@ -6,7 +6,7 @@ from os.path import dirname, isfile, join
 
 import pytest
 
-from tests.helper import enablePrintStatementsForTest, readJson
+from tests.helper import readJson
 
 
 class TestData(unittest.TestCase):
@@ -21,11 +21,49 @@ class TestData(unittest.TestCase):
             'heating.boiler.temperature'  # ignore as value is to low to be plausible in response data
         ]
 
+        all_features = self.read_all_features()
+        all_python_files = self.read_all_python_code()
+
+        missing_features = {}
+        for feature in all_features:
+            found = self.find_feature_in_code(all_python_files, feature)
+
+            foundInFiles = all_features[feature]['files']
+            if not found and len(foundInFiles) > 0 and feature not in ignore:
+                missing_features[feature] = foundInFiles
+
+        has_missing_features = len(missing_features) >= 0
+        self.assertFalse(has_missing_features, json.dumps(missing_features, sort_keys=True, indent=2))
+
+    def find_feature_in_code(self, all_python_files, feature):
+        search_string = f'[\'"]{feature}[\'"]'.replace(".", r"\.")
+        search_string = re.sub(r"\d", r"{.*?}", search_string)
+        search_string = re.sub(r'\\.modes\\.\w+', r'\\.modes\\.\\w+', search_string)
+        search_string = re.sub(r'\\.programs\\.\w+', r'\\.programs\\.\\w+', search_string)
+
+        found = False
+        for search_python in all_python_files:
+            if re.search(search_string, all_python_files[search_python]):
+                found = True
+                break
+        return found
+
+    def read_all_python_code(self):
+        python_path = join(dirname(__file__), '../PyViCare')
+        python_files = [f for f in listdir(python_path) if isfile(join(python_path, f))]
+
+        all_python_files = {}
+
+        for python in python_files:
+            with open(join(python_path, python)) as f:
+                all_python_files[python] = f.read()
+        return all_python_files
+
+    def read_all_features(self):
         response_path = join(dirname(__file__), './response')
         response_files = [f for f in listdir(response_path) if isfile(join(response_path, f))]
 
         all_features = {}
-
         for response in response_files:
             data = readJson(join(response_path, response))
             if "data" in data:
@@ -36,34 +74,4 @@ class TestData(unittest.TestCase):
 
                     if feature['isEnabled'] and feature['properties'] != {} and feature['components'] == []:
                         all_features[name]['files'].append(response)
-
-        python_path = join(dirname(__file__), '../PyViCare')
-        python_files = [f for f in listdir(python_path) if isfile(join(python_path, f))]
-
-        all_python_files = {}
-
-        for python in python_files:
-            with open(join(python_path, python)) as f:
-                all_python_files[python] = f.read()
-
-        missing_features = {}
-        for feature in all_features:
-            search_string = f'[\'"]{feature}[\'"]'.replace(".", r"\.")
-            search_string = re.sub(r"\d", r"{.*?}", search_string)
-            search_string = re.sub(r'\\.modes\\.\w+', r'\\.modes\\.\\w+', search_string)
-            search_string = re.sub(r'\\.programs\\.\w+', r'\\.programs\\.\\w+', search_string)
-
-            # print(search_string)
-            found = False
-            for search_python in all_python_files:
-
-                if re.search(search_string, all_python_files[search_python]):
-                    found = True
-                    break
-
-            foundInFiles = all_features[feature]['files']
-            if not found and len(foundInFiles) > 0 and feature not in ignore:
-                missing_features[feature] = foundInFiles
-
-        has_missing_features = len(missing_features) >= 0
-        self.assertFalse(has_missing_features, json.dumps(missing_features, sort_keys=True, indent=2))
+        return all_features
