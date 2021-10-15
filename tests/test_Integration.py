dd925971ffd96b6a56ab5d8c6dc798cb3fc3f3d9
Lukas Wöhrl <lukas@woehrl.net>
2021-10-15 22:01:07 +0000

    fix some linter issues

--- tests/test_Integration.py
+++ tests/test_Integration.py
@@ -12,15 +12,15 @@ EXEC_INTEGRATION_TEST = int(os.getenv('EXEC_INTEGRATION_TEST', '0'))
 TOKEN_FILE = "browser.save"
 
 
-def allGetterMethods(object):
-    for method_name in dir(object):
+def all_getter_methods(obj):
+    for method_name in dir(obj):
         if method_name.startswith("get"):
-            method = getattr(object, method_name)
+            method = getattr(obj, method_name)
             if callable(method):
                 yield (method_name, method)
 
 
-def prettyPrintResults(result):
+def pretty_print_results(result):
     # format dictionary and lists nicely
     if isinstance(result, dict) or isinstance(result, list):
         formatted = json.dumps(result, sort_keys=True, indent=2)
@@ -30,11 +30,11 @@ def prettyPrintResults(result):
         return result
 
 
-def dumpResults(vicare_device):
-    for (name, method) in allGetterMethods(vicare_device):
+def dump_results(vicare_device):
+    for (name, method) in all_getter_methods(vicare_device):
         result = None
         try:
-            result = prettyPrintResults(method())
+            result = pretty_print_results(method())
         except TypeError:  # skip methods which have more than one argument
             result = "Skipped"
         except PyViCareNotSupportedFeatureError:
@@ -62,33 +62,33 @@ class Integration(unittest.TestCase):
 
             vicare = create_client()
 
-            print("Found %s devices" % len(vicare.devices))
+            print(f"Found {len(vicare.devices)} devices")
 
-            for deviceConfig in vicare.devices:
+            for device_config in vicare.devices:
                 print()
-                print(f"{'model':<45}{deviceConfig.getModel()}")
-                print(f"{'isOnline':<45}{deviceConfig.isOnline()}")
+                print(f"{'model':<45}{device_config.getModel()}")
+                print(f"{'isOnline':<45}{device_config.isOnline()}")
 
-                device = deviceConfig.asAutoDetectDevice()
+                device = device_config.asAutoDetectDevice()
                 auto_type_name = type(device).__name__
                 print(f"{'detected type':<45}{auto_type_name}")
 
-                dumpResults(device)
+                dump_results(device)
                 print()
 
                 for circuit in device.circuits:
                     print(f"{'Use circuit':<45}{circuit.id}")
-                    dumpResults(circuit)
+                    dump_results(circuit)
                     print()
 
                 for burner in device.burners:
                     print(f"{'Use burner':<45}{burner.id}")
-                    dumpResults(burner)
+                    dump_results(burner)
                     print()
 
                 for compressor in device.compressors:
                     print(f"{'Use compressor':<45}{compressor.id}")
-                    dumpResults(compressor)
+                    dump_results(compressor)
                     print()
 
             print()
@@ -109,12 +109,12 @@ class Integration(unittest.TestCase):
                         print(d.createdAt)
 
     @unittest.skipIf(not EXEC_INTEGRATION_TEST, "environments needed")
-    def test_Dump(self):
+    def test_dump(self):
         with enablePrintStatementsForTest(self):
             vicare = vicare = create_client()
 
-            with open("dump.json", mode='w') as output:
+            with open("dump.json", mode='w', encoding="utf-8") as output:
                 output.write(vicare.devices[0].dump_secure())
 
-            with open("dump.flat.json", mode='w') as output:
+            with open("dump.flat.json", mode='w', encoding="utf-8") as output:
                 output.write(vicare.devices[0].dump_secure(flat=True))
