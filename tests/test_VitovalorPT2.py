70356b53ab7d0bf0982dd6081bc952507b8dd15b
Patrick Nagel <mail@patrick-nagel.net>
2022-11-18 16:33:25 +0000

    Added tests for new PyViCareFuelCell.py functions

--- tests/test_VitovalorPT2.py
+++ tests/test_VitovalorPT2.py
@@ -52,3 +52,91 @@ class VitovalorPT2(unittest.TestCase):
     def test_getGasConsumptionTotalDays(self):
         expected_consumption = [1.5, 0.2, 0.30000000000000004, 0.2, 0.4, 0.6000000000000001, 1.7, 0.2]
         self.assertListEqual(self.device.getGasConsumptionTotalDays(), expected_consumption)
+
+    def test_getFuelCellOperatingModeActive(self):
+        self.assertEqual(self.device.getFuelCellOperatingModeActive(), "economical")
+
+    def test_getFuelCellPowerProductionUnit(self):
+        # Returns the unit for the fuel cell's power production statistics, e.g. kilowattHour
+        self.assertEqual(self.device.getFuelCellPowerProductionUnit(), "kilowattHour")
+
+    def test_getFuelCellPowerProductionDays(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionDays(), [0.8, 18.3, 13, 15.1, 16.5, 18.3, 8.6, 18.3])
+
+    def test_getFuelCellPowerProductionToday(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionToday(), 0.8)
+
+    def test_getFuelCellPowerProductionWeeks(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionWeeks(), [63.7, 95.80000000000001, 60.9, 40.2, 41.300000000000004, 40.6, 58.699999999999996])
+
+    def test_getFuelCellPowerProductionThisWeek(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionThisWeek(), 63.7)
+
+    def test_getFuelCellPowerProductionMonths(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionMonths(), [212.6, 206.7, 190, 130.4, 39.9, 0, 187.4, 327.2, 411.3, 441.2, 500.7, 460.5, 431.4])
+
+    def test_getFuelCellPowerProductionThisMonth(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionThisMonth(), 212.6)
+
+    def test_getFuelCellPowerProductionYears(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionYears(), [2647.4, 1609.2])
+
+    def test_getFuelCellPowerProductionThisYear(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionThisYear(), 2647.4)
+
+    def test_getFuelCellOperatingPhase(self):
+        self.assertEqual(self.device.getFuelCellOperatingPhase(), "standby")
+
+    def test_getFuelCellPowerProductionCurrentUnit(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionCurrentUnit(), "watt")
+
+    def test_getFuelCellPowerProductionCurrent(self):
+        self.assertEqual(self.device.getFuelCellPowerProductionCurrent(), 0)
+
+    def test_getFuelCellFlowReturnTemperatureUnit(self):
+        self.assertEqual(self.device.getFuelCellFlowReturnTemperatureUnit(), "celsius")
+
+    def test_getFuelCellFlowReturnTemperature(self):
+        self.assertEqual(self.device.getFuelCellFlowReturnTemperature(), 33.9)
+
+    def test_getFuelCellFlowSupplyTemperatureUnit(self):
+        self.assertEqual(self.device.getFuelCellFlowSupplyTemperatureUnit(), "celsius")
+
+    def test_getFuelCellFlowSupplyTemperature(self):
+        self.assertEqual(self.device.getFuelCellFlowSupplyTemperature(), 39)
+
+    def test_getFuelCellOperationHours(self):
+        self.assertEqual(self.device.getFuelCellOperationHours(), 11594)
+
+    def test_getFuelCellProductionHours(self):
+        self.assertEqual(self.device.getFuelCellProductionHours(), 5980)
+
+    def test_getFuelCellProductionStarts(self):
+        self.assertEqual(self.device.getFuelCellProductionStarts(), 383)
+
+    def test_getFuelCellGasConsumptionUnit(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionUnit(), "cubicMeter")
+
+    def test_getFuelCellGasConsumptionDays(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionDays(), [1.5, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+
+    def test_getFuelCellGasConsumptionToday(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionToday(), 1.5)
+
+    def test_getFuelCellGasConsumptionWeeks(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionWeeks(), [2.3000000000000003, 1.4, 1.4, 1.4, 1.4, 1.4, 1.4])
+
+    def test_getFuelCellGasConsumptionThisWeek(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionThisWeek(), 2.3000000000000003)
+
+    def test_getFuelCellGasConsumptionMonths(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionMonths(), [4.9, 55.5, 50.9, 35.1, 10.9, 0, 49.4, 85.4, 108.5, 118, 132.5, 120.7, 111.5])
+
+    def test_getFuelCellGasConsumptionThisMonth(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionThisMonth(), 4.9)
+
+    def test_getFuelCellGasConsumptionYears(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionYears(), [651.1, 416.6])
+
+    def test_getFuelCellGasConsumptionThisYear(self):
+        self.assertEqual(self.device.getFuelCellGasConsumptionThisYear(), 651.1)
