2b1086fb6d153bbaf196f5e2cbd263bbe1e1481a
Werner Kuehn <werner_kuehn@gmx.de>
2023-05-14 05:50:08 +0200

    Update test_PyViCareService.py

--- tests/test_PyViCareService.py
+++ tests/test_PyViCareService.py
@@ -19,9 +19,9 @@ class PyViCareServiceTest(unittest.TestCase):
     def test_setProperty_object(self):
         self.service.setProperty("someprop", "doaction", {'name': 'abc'})
         self.oauth_mock.post.assert_called_once_with(
-            '/features/installations/[id]/gateways/[serial]/devices/[device]/features/someprop/doaction', '{"name": "abc"}')
+            '/features/installations/[id]/gateways/[serial]/devices/[device]/features/someprop/commands/doaction', '{"name": "abc"}')
 
     def test_setProperty_string(self):
         self.service.setProperty("someprop", "doaction", '{}')
         self.oauth_mock.post.assert_called_once_with(
-            '/features/installations/[id]/gateways/[serial]/devices/[device]/features/someprop/doaction', '{}')
+            '/features/installations/[id]/gateways/[serial]/devices/[device]/features/someprop/commands/doaction', '{}')
