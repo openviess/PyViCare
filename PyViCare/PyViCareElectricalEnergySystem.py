f67b42179b8bc37cfc5363f0202f2efdff688858
Tobin Rosenau <tobin.rosenau@mailbox.org>
2023-05-27 12:12:57 +0000

    add a missing Property and format the File

--- PyViCare/PyViCareElectricalEnergySystem.py
+++ PyViCare/PyViCareElectricalEnergySystem.py
@@ -1,111 +1,156 @@
 from typing import Any, List
 
-from PyViCare.PyViCareHeatingDevice import (HeatingDevice,
-                                            get_available_burners)
+from PyViCare.PyViCareHeatingDevice import HeatingDevice, get_available_burners
 from PyViCare.PyViCareUtils import handleNotSupported
 
-class ElectricalEnergySystem(HeatingDevice):
 
+class ElectricalEnergySystem(HeatingDevice):
     @handleNotSupported
     def getSerial(self):
         return self.service.getProperty("device.serial")["properties"]["value"]["value"]
 
     @handleNotSupported
     def getPointOfCommonCouplingTransferPowerExchange(self):
-        return self.service.getProperty("pcc.transfer.power.exchange")["properties"]["value"]["value"]
-    
+        return self.service.getProperty("pcc.transfer.power.exchange")["properties"][
+            "value"
+        ]["value"]
+
     @handleNotSupported
     def getPhotovoltaicProductionCumulatedUnit(self):
-        return self.service.getProperty("photovoltaic.production.cumulated")["properties"]["currentDay"]["unit"]
-    
+        return self.service.getProperty("photovoltaic.production.cumulated")[
+            "properties"
+        ]["currentDay"]["unit"]
+
     @handleNotSupported
     def getPhotovoltaicProductionCumulatedCurrentDay(self):
-        return self.service.getProperty("photovoltaic.production.cumulated")["properties"]["currentDay"]["value"]
-    
+        return self.service.getProperty("photovoltaic.production.cumulated")[
+            "properties"
+        ]["currentDay"]["value"]
+
     @handleNotSupported
     def getPhotovoltaicProductionCumulatedCurrentWeek(self):
-        return self.service.getProperty("photovoltaic.production.cumulated")["properties"]["currentWeek"]["value"]
-    
+        return self.service.getProperty("photovoltaic.production.cumulated")[
+            "properties"
+        ]["currentWeek"]["value"]
+
     @handleNotSupported
     def getPhotovoltaicProductionCumulatedCurrentMonth(self):
-        return self.service.getProperty("photovoltaic.production.cumulated")["properties"]["currentMonth"]["value"]
-    
+        return self.service.getProperty("photovoltaic.production.cumulated")[
+            "properties"
+        ]["currentMonth"]["value"]
+
     @handleNotSupported
     def getPhotovoltaicProductionCumulatedCurrentYear(self):
-        return self.service.getProperty("photovoltaic.production.cumulated")["properties"]["currentYear"]["value"]
-    
+        return self.service.getProperty("photovoltaic.production.cumulated")[
+            "properties"
+        ]["currentYear"]["value"]
+
     @handleNotSupported
     def getPhotovoltaicProductionCumulatedLifeCycle(self):
-        return self.service.getProperty("photovoltaic.production.cumulated")["properties"]["lifeCycle"]["value"]
-    
+        return self.service.getProperty("photovoltaic.production.cumulated")[
+            "properties"
+        ]["lifeCycle"]["value"]
+
     @handleNotSupported
     def getPhotovoltaicStatus(self):
-        return self.service.getProperty("photovoltaic.status")["properties"]["status"]["value"]
-    
+        return self.service.getProperty("photovoltaic.status")["properties"]["status"][
+            "value"
+        ]
+
     @handleNotSupported
     def getPhotovoltaicProductionCurrent(self):
-        return self.service.getProperty("photovoltaic.production.current")["properties"]["value"]["value"]
+        return self.service.getProperty("photovoltaic.production.current")[
+            "properties"
+        ]["value"]["value"]
     
-    
+    @handleNotSupported
+    def getPhotovoltaicProductionCurrentUnit(self):
+        return self.service.getProperty("photovoltaic.production.current")[
+            "properties"
+        ]["value"]["unit"]
+
     @handleNotSupported
     def getPointOfCommonCouplingTransferConsumptionTotal(self):
-        return self.service.getProperty("pcc.transfer.consumption.total")["properties"]["value"]["value"]
-    
+        return self.service.getProperty("pcc.transfer.consumption.total")["properties"][
+            "value"
+        ]["value"]
+
     @handleNotSupported
     def getPointOfCommonCouplingTransferConsumptionTotalUnit(self):
-        return self.service.getProperty("pcc.transfer.consumption.total")["properties"]["value"]["unit"]
-    
+        return self.service.getProperty("pcc.transfer.consumption.total")["properties"][
+            "value"
+        ]["unit"]
+
     @handleNotSupported
     def getPointOfCommonCouplingTransferFeedInTotal(self):
-        return self.service.getProperty("pcc.transfer.feedIn.total")["properties"]["value"]["value"]
-    
+        return self.service.getProperty("pcc.transfer.feedIn.total")["properties"][
+            "value"
+        ]["value"]
+
     @handleNotSupported
     def getPointOfCommonCouplingTransferFeedInTotalUnit(self):
-        return self.service.getProperty("pcc.transfer.feedIn.total")["properties"]["value"]["unit"]
-    
+        return self.service.getProperty("pcc.transfer.feedIn.total")["properties"][
+            "value"
+        ]["unit"]
+
     @handleNotSupported
     def getElectricalEnergySystemTransferDischargeCumulatedUnit(self):
-        return self.service.getProperty("ess.transfer.discharge.cumulated")["properties"]["currentDay"]["unit"]
-    
+        return self.service.getProperty("ess.transfer.discharge.cumulated")[
+            "properties"
+        ]["currentDay"]["unit"]
+
     @handleNotSupported
     def getElectricalEnergySystemTransferDischargeCumulatedCurrentDay(self):
-        return self.service.getProperty("ess.transfer.discharge.cumulated")["properties"]["currentDay"]["value"]
-    
+        return self.service.getProperty("ess.transfer.discharge.cumulated")[
+            "properties"
+        ]["currentDay"]["value"]
+
     @handleNotSupported
     def getElectricalEnergySystemTransferDischargeCumulatedCurrentWeek(self):
-        return self.service.getProperty("ess.transfer.discharge.cumulated")["properties"]["currentWeek"]["value"]
-    
+        return self.service.getProperty("ess.transfer.discharge.cumulated")[
+            "properties"
+        ]["currentWeek"]["value"]
+
     @handleNotSupported
     def getElectricalEnergySystemTransferDischargeCumulatedCurrentMonth(self):
-        return self.service.getProperty("ess.transfer.discharge.cumulated")["properties"]["currentMonth"]["value"]
-    
+        return self.service.getProperty("ess.transfer.discharge.cumulated")[
+            "properties"
+        ]["currentMonth"]["value"]
+
     @handleNotSupported
     def getElectricalEnergySystemTransferDischargeCumulatedCurrentYear(self):
-        return self.service.getProperty("ess.transfer.discharge.cumulated")["properties"]["currentYear"]["value"]
-    
+        return self.service.getProperty("ess.transfer.discharge.cumulated")[
+            "properties"
+        ]["currentYear"]["value"]
+
     @handleNotSupported
     def getElectricalEnergySystemTransferDischargeCumulatedLifeCycle(self):
-        return self.service.getProperty("ess.transfer.discharge.cumulated")["properties"]["lifeCycle"]["value"]
-    
+        return self.service.getProperty("ess.transfer.discharge.cumulated")[
+            "properties"
+        ]["lifeCycle"]["value"]
+
     @handleNotSupported
     def getElectricalEnergySystemSOC(self):
-        return self.service.getProperty("ess.stateOfCharge")["properties"]["value"]["value"]
-    
+        return self.service.getProperty("ess.stateOfCharge")["properties"]["value"][
+            "value"
+        ]
+
     @handleNotSupported
     def getElectricalEnergySystemSOCUnit(self):
-        return self.service.getProperty("ess.stateOfCharge")["properties"]["value"]["unit"]
-    
+        return self.service.getProperty("ess.stateOfCharge")["properties"]["value"][
+            "unit"
+        ]
+
     @handleNotSupported
     def getElectricalEnergySystemPower(self):
         return self.service.getProperty("ess.power")["properties"]["value"]["value"]
-    
+
     @handleNotSupported
     def getElectricalEnergySystemPowerUnit(self):
         return self.service.getProperty("ess.power")["properties"]["value"]["value"]
-    
+
     @handleNotSupported
     def getElectricalEnergySystemOperationState(self):
-        return self.service.getProperty("ess.operationState")["properties"]["value"]["value"]
-    
-
-    
+        return self.service.getProperty("ess.operationState")["properties"]["value"][
+            "value"
+        ]
