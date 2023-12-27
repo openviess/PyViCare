8a166c4ba0cca06bcf8df3cae447107c837fe6be
Christopher Fenner <christopher.fenner@me.com>
2023-12-27 09:12:51 +0100

    change test case

--- tests/test_PyViCareService.py
+++ tests/test_PyViCareService.py
@@ -2,15 +2,14 @@ import unittest
 from unittest.mock import Mock
 
 from PyViCare.PyViCareService import ViCareDeviceAccessor, ViCareService
-from .test_PyViCareDeviceConfig import has_roles
 
 
 class PyViCareServiceTest(unittest.TestCase):
 
     def setUp(self):
         self.oauth_mock = Mock()
-        accessor = ViCareDeviceAccessor("[id]", "[serial]", "[device]")
-        self.service = ViCareService(self.oauth_mock, accessor, [])
+        self.accessor = ViCareDeviceAccessor("[id]", "[serial]", "[device]")
+        self.service = ViCareService(self.oauth_mock, self.accessor, [])
 
     def test_getProperty(self):
         self.service.getProperty("someprop")
@@ -28,7 +27,7 @@ class PyViCareServiceTest(unittest.TestCase):
             '/features/installations/[id]/gateways/[serial]/devices/[device]/features/someprop/commands/doaction', '{}')
 
     def test_getProperty_gateway(self):
-        self.service.hasRoles = has_roles(["type:gateway;VitoconnectOpto1"])
+        self.service = ViCareService(self.oauth_mock, self.accessor, ["type:gateway;VitoconnectOpto1"])
         self.service.getProperty("someprop")
         self.oauth_mock.get.assert_called_once_with(
             '/features/installations/[id]/gateways/[serial]/features/someprop')
