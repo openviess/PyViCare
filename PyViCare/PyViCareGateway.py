2afa501d7272c4b1058b355e653a9b7d7dcb0f79
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2024-02-18 08:38:09 +0100

    Add getSerial for gateways (#354)
    * Update PyViCareGateway.py
    
    * Update test_VitoconnectOpto1.py
    
    * Apply suggestions from code review

--- PyViCare/PyViCareGateway.py
+++ PyViCare/PyViCareGateway.py
@@ -5,5 +5,9 @@ from PyViCare.PyViCareUtils import handleNotSupported
 class Gateway(Device):
 
     @handleNotSupported
+    def getSerial(self):
+        return self.service.getProperty("gateway.devices")["gatewayId"]
+
+    @handleNotSupported
     def getWifiSignalStrength(self):
         return self.service.getProperty("gateway.wifi")["properties"]["strength"]["value"]
