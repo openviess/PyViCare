d40712c9d70a4d8f52516cfa4434526f38508e5b
Tobin Rosenau <tobin.rosenau@mailbox.org>
2024-03-30 18:52:04 +0000

    add AutoDetect test

--- tests/test_PyViCareDeviceConfig.py
+++ tests/test_PyViCareDeviceConfig.py
@@ -72,8 +72,20 @@ class PyViCareDeviceConfigTest(unittest.TestCase):
         device_type = c.asAutoDetectDevice()
         self.assertEqual("Gateway", type(device_type).__name__)
 
+    def test_autoDetect_TCU300_asGateway(self):
+        c = PyViCareDeviceConfig(self.service, "0", "E3_TCU10_x07", "Online")
+        device_type = c.asAutoDetectDevice()
+        self.assertEqual("Gateway", type(device_type).__name__)
+
     def test_autoDetect_RoleGateway_asGateway(self):
         self.service.hasRoles = has_roles(["type:gateway;VitoconnectOpto1"])
         c = PyViCareDeviceConfig(self.service, "0", "Unknown", "Online")
         device_type = c.asAutoDetectDevice()
         self.assertEqual("Gateway", type(device_type).__name__)
+    
+    def test_autoDetect_RoleGateway_asGateway_TCU300(self):
+        self.service.hasRoles = has_roles(["type:gateway;TCU300"])
+        c = PyViCareDeviceConfig(self.service, "0", "Unknown", "Online")
+        device_type = c.asAutoDetectDevice()
+        self.assertEqual("Gateway", type(device_type).__name__)
+    
