6e77c87479b05d17db59632bc85c8ba1529f34d4
Tobin Rosenau <tobin.rosenau@mailbox.org>
2024-04-02 21:51:56 +0200

    Update PyViCareService.py remove empty lines

--- PyViCare/PyViCareService.py
+++ PyViCare/PyViCareService.py
@@ -8,7 +8,6 @@ from PyViCare.PyViCareUtils import PyViCareNotSupportedFeatureError
 logger = logging.getLogger('ViCare')
 logger.addHandler(logging.NullHandler())
 
-
 def readFeature(entities, property_name):
     feature = next(
         (f for f in entities if f["feature"] == property_name), None)
@@ -18,21 +17,17 @@ def readFeature(entities, property_name):
 
     return feature
 
-
 def hasRoles(requested_roles: List[str], existing_roles: List[str]) -> bool:
     return len(requested_roles) > 0 and set(requested_roles).issubset(set(existing_roles))
 
-
 def buildSetPropertyUrl(accessor, property_name, action):
     return f'/features/installations/{accessor.id}/gateways/{accessor.serial}/devices/{accessor.device_id}/features/{property_name}/commands/{action}'
-
 
 class ViCareDeviceAccessor:
     def __init__(self, _id: int, serial: str, device_id: str) -> None:
         self.id = _id
         self.serial = serial
         self.device_id = device_id
-
 
 class ViCareService:
     def __init__(self, oauth_manager: AbstractViCareOAuthManager, accessor: ViCareDeviceAccessor, roles: List[str]) -> None:
@@ -48,7 +43,6 @@ class ViCareService:
         if self._isGateway():
             return f'/features/installations/{self.accessor.id}/gateways/{self.accessor.serial}/features/{property_name}'
         return f'/features/installations/{self.accessor.id}/gateways/{self.accessor.serial}/devices/{self.accessor.device_id}/features/{property_name}'
-
 
     def hasRoles(self, requested_roles) -> bool:
         return hasRoles(requested_roles, self.roles)
