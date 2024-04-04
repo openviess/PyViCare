90d38a3403752ac1585bcf278c392f896aff66e5
Tobin Rosenau <tobin.rosenau@mailbox.org>
2024-04-04 16:50:02 +0000

    add test and response for Vitoladens 300-C

---
+++ tests/test_Vitoladens300C.py
@@ -0,0 +1,26 @@
+import unittest
+
+from PyViCare.PyViCareOilBoiler import OilBoiler
+from PyViCare.PyViCareUtils import PyViCareNotSupportedFeatureError
+from tests.ViCareServiceMock import ViCareServiceMock
+
+
+class Vitoladens300C(unittest.TestCase):
+    def setUp(self):
+        self.service = ViCareServiceMock('response/Vitoladens300-C_J3RA.json')
+        self.device = OilBoiler(self.service)
+
+    def test_getActive(self):
+        self.assertEqual(self.device.burners[0].getActive(), False)
+
+    def test_getBurnerStarts(self):
+        self.assertEqual(self.device.burners[0].getStarts(), 29098)
+
+    def test_getBurnerHours(self):
+        self.assertEqual(self.device.burners[0].getHours(), 4222.5)
+
+    def test_getBurnerModulation(self):
+        self.assertEqual(self.device.burners[0].getModulation(), 0)
+
+    def test_getBoilerTemperature(self):
+        self.assertEqual(self.device.getBoilerTemperature(), 42)
