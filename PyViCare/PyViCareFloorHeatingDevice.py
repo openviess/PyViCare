f5d362a47a623e0e4832dd0a5475e9054b3f86c3
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2024-05-24 14:26:04 +0200

    Update PyViCareFloorHeatingDevice.py

--- PyViCare/PyViCareFloorHeatingDevice.py
+++ PyViCare/PyViCareFloorHeatingDevice.py
@@ -13,5 +13,23 @@ class RadiatorActuator(HeatingDevice):
         return self.service.getProperty("fht.sensors.temperature.supply")["properties"]["value"]["value"]
 
     @handleNotSupported
+    def getModes(self):
+        return self.service.getProperty("fht.operating.modes.active")["commands"]["setMode"]["params"]["mode"]["constraints"]["enum"]
+
+    @handleNotSupported
     def getActiveMode(self):
         return self.service.getProperty("fht.operating.modes.active")["properties"]["value"]["value"]
+
+    def setMode(self, mode):
+        """ Set the active mode
+        Parameters
+        ----------
+        mode : str
+            Valid mode can be obtained using getModes()
+
+        Returns
+        -------
+        result: json
+            json representation of the answer
+        """
+        return self.service.setProperty("fht.operating.modes.active", "setMode", {'mode': mode})
