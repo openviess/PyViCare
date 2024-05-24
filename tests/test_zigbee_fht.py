fad98d86c2b6ca9481f32b8ea19c335f1f133b14
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2024-05-24 10:24:29 +0200

    Update test_zigbee_fht.py

--- tests/test_zigbee_fht.py
+++ tests/test_zigbee_fht.py
@@ -9,6 +9,10 @@ class ZK03840(unittest.TestCase):
         self.service = ViCareServiceMock('response/zigbee_fht.json')
         self.device = RadiatorActuator(self.service)
 
+    def test_getSerial(self):
+        self.assertEqual(
+            self.device.getSerial(), "zigbee-1a1a1a1a1a1a1a1a")
+
     def test_getTemperature(self):
         self.assertEqual(
             self.device.getTemperature(), 18.5)
