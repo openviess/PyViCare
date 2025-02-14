833177662ef0ea7adbeff7fa4aa86babe2eaee2c
Christopher Fenner <Christopher.Fenner@me.com>
2025-02-14 22:42:55 +0100

    fix imports

--- PyViCare/PyViCarePelletsBoiler.py
+++ PyViCare/PyViCarePelletsBoiler.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any, List
+from typing import List
 
 from PyViCare.PyViCareHeatingDevice import (HeatingDevice, HeatingDeviceWithComponent, get_available_burners)
 from PyViCare.PyViCareUtils import handleNotSupported
