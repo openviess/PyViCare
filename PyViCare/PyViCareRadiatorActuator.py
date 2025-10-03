6f876c2877039f737ade3d7d6042c7984a2c6ef6
fdebrus <fdebrus@hotmail.com>
2025-10-03 10:04:04 +0200

    RadiatorActuator: update TRV datapoints

--- PyViCare/PyViCareRadiatorActuator.py
+++ PyViCare/PyViCareRadiatorActuator.py
@@ -1,7 +1,6 @@
 from PyViCare.PyViCareDevice import Device
 from PyViCare.PyViCareUtils import handleAPICommandErrors, handleNotSupported
 
-
 class RadiatorActuator(Device):
 
     @handleNotSupported
@@ -14,12 +13,27 @@ class RadiatorActuator(Device):
 
     @handleNotSupported
     def getTemperature(self):
-        return self.service.getProperty("device.sensors.temperature")["properties"]["value"]["value"]
+        # Zigbee TRV reports temperature under trv.temperature, not device.sensors.temperature
+        return self.service.getProperty("trv.temperature")["properties"]["value"]["value"]
+
+    @handleNotSupported
+    def getValvePosition(self):
+        return self.service.getProperty("trv.valve.position")["properties"]["position"]["value"]
+
+    @handleNotSupported
+    def getChildLockStatus(self):
+        return self.service.getProperty("trv.childLock")["properties"]["status"]["value"]
+
+    @handleNotSupported
+    def getMountingModeActive(self):
+        return self.service.getProperty("trv.mountingMode")["properties"]["active"]["value"]
+
+    @handleNotSupported
+    def getLinkQuality(self):
+        return self.service.getProperty("device.zigbee.lqi")["properties"]["strength"]["value"]
 
     @handleNotSupported
     def getTargetTemperature(self):
+        # note: this is the *setpoint* value, read-only unless API exposes write
         return self.service.getProperty("trv.temperature")["properties"]["value"]["value"]
 
-    @handleAPICommandErrors
-    def setTargetTemperature(self, temperature):
-        return self.service.setProperty("trv.temperature", "setTargetTemperature", {'temperature': float(temperature)})
