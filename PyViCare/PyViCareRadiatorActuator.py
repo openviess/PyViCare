8681febdbf9bd6e36fd29e1a435c61d1803ee966
fdebrus <33791533+fdebrus@users.noreply.github.com>
2025-10-07 21:51:26 +0200

    feat(trv): add TRV datapoints (#613)
    * RadiatorActuator: update TRV datapoints
    
    * Update PyViCareRadiatorActuator.py
    
    * Update PyViCareRadiatorActuator.py
    
    ---------
    
    Co-authored-by: Christopher Fenner <9592452+CFenner@users.noreply.github.com>

--- PyViCare/PyViCareRadiatorActuator.py
+++ PyViCare/PyViCareRadiatorActuator.py
@@ -17,6 +17,22 @@ class RadiatorActuator(Device):
         return self.service.getProperty("device.sensors.temperature")["properties"]["value"]["value"]
 
     @handleNotSupported
+    def getValvePosition(self) -> int:
+        return int(self.service.getProperty("trv.valve.position")["properties"]["position"]["value"])
+
+    @handleNotSupported
+    def getChildLock(self) -> str:
+        return str(self.service.getProperty("trv.childLock")["properties"]["status"]["value"])
+
+    @handleNotSupported
+    def getMountingMode(self) -> bool:
+        return bool(self.service.getProperty("trv.mountingMode")["properties"]["active"]["value"])
+
+    @handleNotSupported
+    def getZigbeeLinkQuality(self) -> int:
+        return int(self.service.getProperty("device.zigbee.lqi")["properties"]["strength"]["value"])
+
+    @handleNotSupported
     def getTargetTemperature(self):
         return self.service.getProperty("trv.temperature")["properties"]["value"]["value"]
 
