1a565ef312a3275cb3148f523f48c05f975cabb4
Christopher Fenner <Christopher.Fenner@me.com>
2025-10-21 17:17:51 +0200

    fix get_available_burners

--- PyViCare/PyViCareFuelCell.py
+++ PyViCare/PyViCareFuelCell.py
@@ -17,7 +17,7 @@ class FuelCell(HeatingDevice):
 
     @handleNotSupported
     def getAvailableBurners(self):
-        return get_available_burners(self.service)
+        return get_available_burners(self.accessor, self.service)
 
     @handleNotSupported
     def getReturnTemperature(self):
