5b52d16a01ada5973442c7fe71a31fe0a9056056
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2025-10-22 17:54:35 +0200

    Add VentilationDevice to Hybrid class structure

--- PyViCare/PyViCareHybrid.py
+++ PyViCare/PyViCareHybrid.py
@@ -1,6 +1,10 @@
 from PyViCare.PyViCareGazBoiler import GazBoiler
 from PyViCare.PyViCareHeatPump import HeatPump
+from PyViCare.PyViCareVentilationDevice import VentilationDevice
 
 
 class Hybrid(GazBoiler, HeatPump):
+    pass
+
+class HeatPumpWithVentilation(HeatPump, VentilationDevice):
     pass
