726742c5d3e33e3071fa2d39a6cc8c605e2c7b75
Christopher Fenner <Christopher.Fenner@me.com>
2025-11-11 22:21:55 +0100

    add test case

--- tests/test_Vitocal333G_with_Vitovent300F.py
+++ tests/test_Vitocal333G_with_Vitovent300F.py
@@ -15,3 +15,7 @@ class Vitocal333G_with_Vitovent300F(unittest.TestCase):
     def test_getVolumeFlow(self):
         self.assertEqual(self.device.getSupplyVolumeFlow(), 257)
         self.assertEqual(self.device.getExhaustVolumeFlow(), 257)
+
+    def test_CondensorSubcoolingTemperature(self):
+        self.assertEqual(self.device.getCondensor("0").getCondensorSubcoolingTemperature(), -2.8)
+        self.assertEqual(self.device.getCondensor("0").getCondensorSubcoolingTemperatureUnit(), "celsius")
